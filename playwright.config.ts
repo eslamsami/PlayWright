e2283f9621f939add701bc64dbe0e7d2bfb09ac4
Eslam Samy <islam.samy2008@gmail.com>
2026-01-04 14:29:29 +0200

    Updating Yaml file and config

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['list'],
-    ['allure-playwright']
-  ],
+  reporter: [['html'],['allure-playwright']],
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
